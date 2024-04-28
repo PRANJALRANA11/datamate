9606aa223df1a028a326674c1fc659c41b148e42
Pranjal Rana <129268721+PRANJALRANA11@users.noreply.github.com>
2024-04-28 13:36:47 +0530

    adding-tests

--- app.py
+++ app.py
@@ -28,19 +28,23 @@ def main():
     data = DataVisualize()
     kernel = Kernel()
     # Search for the dataset and formating it
+    input("Which dataset you want to load from internet \n")
     res = DataGathering.search_github("Apple stocks")
     format_res = DataGathering.format_items(res)
     # Load the dataset
-    code, output = kernel.execute_code(f"import pandas as pd\ndf = pd.read_csv('{format_res[0]['fullurl']}')\ndf.head()",0)
+    print("loading the dataset...")
+    code, output = kernel.execute_code(f"import pandas as pd\ndf = pd.read_csv('{format_res[0]['fullurl']}')\ndf.head()")
+    print("Apple Stocks dataset loaded successfully!")
     init_code = f"import pandas as pd\ndf = pd.read_csv('{format_res[0]['fullurl']}')\ndf.head()"
+    total_code = init_code
 
-
-    prompt = input("enter the prompt")
+    prompt = input("what would you like to do with dataset \n")
     codeAgent = CodeAgent()
     code = codeAgent.generate_code(prompt,output,init_code)
     refine_code = RefineCode(code)
     code = refine_code.refine()
-    code, output = kernel.execute_code(code[1],1)
+    total_code += code[1]
+    code, output = kernel.execute_code(total_code)
     if output != 'image_path':
         codeAgent.generate_code(prompt,output)
     else:
@@ -48,24 +52,26 @@ def main():
     # Give the prompt to generate refine and execute the code
     for i in range(4):
         if output == "imageToSaved.png":
-            print("visual")
-            prompt = input("enter the prompt")
+            # print("visual")
+            prompt = input("what would you like to do more with dataset \n")
             code =data.generate_gemini_response(prompt,output)
             refine_code = RefineCode(code)
             code = refine_code.refine()
-            code, output = kernel.execute_code(code[1])
+            total_code += code[1]
+            code, output = kernel.execute_code(total_code)
             if output != 'imageToSaved.png':
                 codeAgent.generate_code(prompt,output)
             else:
                 data.generate_gemini_response(prompt,output)
         else:
-            print("code")
-            prompt = input("enter the prompt")
+            # print("code")
+            prompt = input("what would you like to do with dataset \n")
             codeAgent = CodeAgent()
             code = codeAgent.generate_code(prompt,output)
             refine_code = RefineCode(code)
             code = refine_code.refine()
-            code, output = kernel.execute_code(code[1])
+            total_code += code[1]
+            code, output = kernel.execute_code(total_code)
             if output != 'image_path':
                 codeAgent.generate_code(prompt,output)
             else:
